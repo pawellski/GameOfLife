cfd1c1fd7c725f440ce75f84fbf2128ffb93ade6
J-Swider <jakub.swider999@gmail.com>
2019-03-31 11:29:50 +0200

    Dodanie funkji change() do modułu Game

--- Game.c
+++ Game.c
@@ -11,3 +11,11 @@ void to_clear(grid_t *toclear, dimension_t *dim)
             toclear->cells[i][j] = '0';
     }
 }
+void change(grid_t *first, grid_t *second, dimension_t *dim)
+{
+    for(int i = 0; i<(dim->rows); i++)
+    {
+        for(int j = 0; j<(dim->columns); j++)
+            first->cells[i][j] = second->cells[i][j];
+    }
+}
--- Game.h
+++ Game.h
@@ -1,3 +1,4 @@
 #include "Struct.h"
 
 void to_clear(grid_t *toclear, dimension_t *dim);
+void change(grid_t *first, grid_t *second, dimension_t *dim);
