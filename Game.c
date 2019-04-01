5b7bc05b2f49c4e3429db3b715dd788622260a27
pawellski <49031668+pawellski@users.noreply.github.com>
2019-04-02 01:15:31 +0200

    Dodanie flag oraz struktury stanów komórek

--- Game.c
+++ Game.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "WriteFile.h"
 #include "Game.h"
+#include "Struct.h"
 
 
-void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *main_grid, grid_t *util_grid)
+void generation(int n, int writeOpt, char *fileOut, dimension_t *dim, grid_t *main_grid, grid_t *util_grid, symbols_t * syms)
 {
+    symbols_t syms2;
+    syms2.alive = syms->alive;
+    syms2.dead = syms->dead;
     char **ptr = (char*)malloc((dim->rows)*sizeof(char*));
     for(int i = 0; i<(dim->rows); i++)
         ptr[i] = &(main_grid->cells[i][0]);
@@ -16,19 +19,19 @@ void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *ma
         for(int i=1; i<dim->rows-1; i++)
         {
             for(int j=1; j<dim->columns-1; j++)
-                util_grid->cells[i][j] = is_alive(i, j, ptr);
+                util_grid->cells[i][j] = is_alive(i, j, ptr, &syms2);
         }
-        switch(write)
+		switch(writeOpt)
         {
         case 0:
             print(it, util_grid, dim);
             break;
         case 1:
-            fill_in_file(it, write_file, util_grid, dim);
+            fill_in_file(it, fileOut, util_grid, dim);
             break;
         case 2:
             print(it, util_grid, dim);
-            fill_in_file(it, write_file, util_grid, dim);
+            fill_in_file(it, fileOut, util_grid, dim);
         default:
             break;
         }
@@ -40,45 +43,45 @@ void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *ma
             printf("Plansza nie zmienia sie od %d generacji\n", it-1);
             break;
         }
-        to_clear(main_grid, dim);
-        change(main_grid, util_grid, dim);
-        to_clear(util_grid, dim);
+        to_clear( main_grid, dim, &syms2 );
+        change( main_grid, util_grid, dim );
+        to_clear( util_grid, dim, &syms2 );
     }
 }
 
-char is_alive(int i, int j, char **main_ptr)
+char is_alive(int i, int j, char **main_ptr, symbols_t * syms)
 {
     int alive_number = 0;
     for(int a=-1; a<2; a++)
     {
         for(int b=-1; b<2; b++)
-            if(main_ptr[i+a][j+b] == '1')
+            if(main_ptr[i+a][j+b] == syms->alive)
                 alive_number++;
     }
 
-    if(main_ptr[i][j] == '1')
+    if(main_ptr[i][j] == syms->alive)
     {
         alive_number--;
         if(alive_number == 2 || alive_number == 3)
-            return '1';
+            return syms->alive;
         else
-            return '0';
+            return syms->dead;
     }
     else
     {
         if(alive_number == 3)
-            return '1';
+            return syms->alive;
         else
-            return '0';
+            return syms->dead;
     }
 }
 
-void to_clear(grid_t *toclear, dimension_t *dim)
+void to_clear(grid_t *toclear, dimension_t *dim, symbols_t * syms)
 {
     for(int i = 0; i < dim->rows; i++)
     {
         for(int j = 0; j < dim->columns; j++)
-            toclear->cells[i][j] = '0';
+            toclear->cells[i][j] = syms->dead;
     }
 }
 
@@ -90,6 +93,7 @@ void change(grid_t *first, grid_t *second, dimension_t *dim)
             first->cells[i][j] = second->cells[i][j];
     }
 }
+
 int check(grid_t *check_1, grid_t *check_2, dimension_t *dim)
 {
     int count = 0;
@@ -106,3 +110,4 @@ int check(grid_t *check_1, grid_t *check_2, dimension_t *dim)
     else
         return 1;
 }
+
--- Game.h
+++ Game.h
@@ -2,11 +2,11 @@
 #define GAME_H_INCLUDED
 
 #include "Struct.h"
-char is_alive(int i, int j, char **main_ptr);
-int check(grid_t *check_1, grid_t *check_2, dimension_t *dim);
-void change(grid_t *first, grid_t *second, dimension_t *dim);
-void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *main_grid, grid_t *util_grid);
-void to_clear(grid_t *toclear, dimension_t *dim);
 
+char is_alive( int i, int j, char ** ptr, symbols_t * syms );
+void generation( int n, int writeOpt, char *fileOut, dimension_t *dim, grid_t *main_grid, grid_t *util_grid, symbols_t * syms );
+void to_clear( grid_t *toclear, dimension_t *dim, symbols_t * syms );
+void change( grid_t *first, grid_t *second, dimension_t *dim );
+int check( grid_t *check_1, grid_t *check_2, dimension_t *dim );
 
 #endif // GAME_H_INCLUDED
