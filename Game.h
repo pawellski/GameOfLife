94841a9d5d0ba642a5ac34cd02c4025e5d1c121a
J-Swider <jakub.swider999@gmail.com>
2019-04-01 14:24:00 +0200

    Poprawienie pliku nagłówkowego Game.h

--- Game.h
+++ Game.h
@@ -1,7 +1,6 @@
 #include "Struct.h"
-
-void to_clear(grid_t *toclear, dimension_t *dim);
-void change(grid_t *first, grid_t *second, dimension_t *dim);
 char is_alive(int i, int j, char **main_ptr);
-void generation(int n, dimension_t *dim, grid_t *main_grid, grid_t *util_grid);
-
+int check(grid_t *check_1, grid_t *check_2, dimension_t *dim);
+void change(grid_t *first, grid_t *second, dimension_t *dim);
+void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *main_grid, grid_t *util_grid);
+void to_clear(grid_t *toclear, dimension_t *dim);
