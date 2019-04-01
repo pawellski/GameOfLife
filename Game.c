a83eb2cde66b421972312708ce2f566d95d0a283
J-Swider <jakub.swider999@gmail.com>
2019-04-01 13:41:54 +0200

    Uzupełnienie main.c i Game.c

--- Game.c
+++ Game.c
@@ -3,7 +3,8 @@
 
 #include "Game.h"
 
-void generation(int n, dimension_t *dim, grid_t *main_grid, grid_t *util_grid)
+
+void generation(int n, int write, char *write_file, dimension_t *dim, grid_t *main_grid, grid_t *util_grid)
 {
     char **ptr = (char*)malloc((dim->rows)*sizeof(char*));
     for(int i = 0; i<(dim->rows); i++)
@@ -16,17 +17,29 @@ void generation(int n, dimension_t *dim, grid_t *main_grid, grid_t *util_grid)
             for(int j=1; j<dim->columns-1; j++)
                 util_grid->cells[i][j] = is_alive(i, j, ptr);
         }
+        switch(write)
+        {
+        case 0:
+            print(it, util_grid, dim);
+            break;
+        case 1:
+            fill_in_file(it, write_file, util_grid, dim);
+            break;
+        case 2:
+            print(it, util_grid, dim);
+            fill_in_file(it, write_file, util_grid, dim);
+        default:
+            break;
+        }
 
-   	printf("PLANSZA PO %d ITERACJI\n", it);
-    	for(int i=1; i<(dim->rows)-1; ++i)
-    	{
-       	    for(int j=1; j<(dim->columns)-1; ++j)
-            {
-            	printf("%c ",util_grid->cells[i][j]);
-       	    }
-            printf("\n");
-    	}
-	to_clear(main_grid, dim);
+        if(check(main_grid, util_grid, dim) == 1)
+            ;
+        else
+        {
+            printf("Plansza nie zmienia sie od %d generacji\n", it-1);
+            break;
+        }
+        to_clear(main_grid, dim);
         change(main_grid, util_grid, dim);
         to_clear(util_grid, dim);
     }
@@ -75,4 +88,20 @@ void change(grid_t *first, grid_t *second, dimension_t *dim)
         for(int j = 0; j<(dim->columns); j++)
             first->cells[i][j] = second->cells[i][j];
     }
+}
+int check(grid_t *check_1, grid_t *check_2, dimension_t *dim)
+{
+    int count = 0;
+    for(int i=1; i<dim->rows-1; i++)
+    {
+        for(int j=1; j<dim->columns-1; j++)
+        {
+            if(check_1->cells[i][j] == check_2->cells[i][j])
+                count++;
+        }
+    }
+    if(count == ((dim->rows-2)*(dim->columns-2)))
+        return 0;
+    else
+        return 1;
 }
--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "ReadFile.h"
+#include "Game.h"
 #include "Struct.h"
 
 int main(int argc, char **argv)
 {
+    int count_generation = atoi(argv[2]);
+    int write = atoi(argv[3]);
+    char write_file = argv[4];
+
     grid_t first_grid;
     grid_t second_grid;
     dimension_t static_dimension;
-
-    int count_generation = atoi(argv[2]);
 
     read_dimension(argv[1], &static_dimension);
 
@@ -25,26 +29,20 @@ int main(int argc, char **argv)
         second_grid.cells[i] = (char*)malloc(static_dimension.columns * sizeof(char));
     }
 
+    //CZYSZCZENIE TABLIC + ROBIENIE BUFORA
+    to_clear(&first_grid, &static_dimension);
+    to_clear(&second_grid, &static_dimension);
 
     //WCZYTANIE PLIKU
     fill_in_grid(argv[1], &first_grid);
 
     //WYSWIETLENIE WCZYTANEJ PLANSZY
-    print(&first_grid, &static_dimension);
-	
-    //WYCZYSZCZENIE PLANSZY
-    to_clear(&second_grid, &static_dimension);
-	
-    // WYŒWIETLENIE PLANSZY
-    printf("Wyczyszczony plik\n");
-   
-   //WYSWIETLENIE WCZYTANEJ PLANSZY
-    print(&second_grid, &static_dimension);
+    print(0, &first_grid, &static_dimension);
 
     //URUCHOMIENIE GRY
-    generation(count_generation, &static_dimension, &first_grid, &second_grid);
+    generation(count_generation, write, argv[4], &static_dimension, &first_grid, &second_grid);
 
-    //ZWOLNIENIE PAMIECI
+    //ZWOLNIENIE PAMIÊCI
     for(int i=0; i<static_dimension.rows; i++)
     {
         free(first_grid.cells[i]);
@@ -55,6 +53,7 @@ int main(int argc, char **argv)
     free(second_grid.cells);
     first_grid.cells = NULL;
     second_grid.cells = NULL;
+
 
     return 0;
 }
