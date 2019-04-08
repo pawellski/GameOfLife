e0ef20d54aaea82406e6156d1b2d45435c228014
J-Swider <jakub.swider999@gmail.com>
2019-04-08 14:15:39 +0200

    Dodanie funkcji innit_grid i free_grid

--- Game.h
+++ Game.h
@@ -8,5 +8,7 @@ void generation( int n, int writeOpt, char *fileOut, dimension_t *dim, grid_t *m
 void to_clear( grid_t *toclear, dimension_t *dim, symbols_t * syms );
 void change( grid_t *first, grid_t *second, dimension_t *dim );
 int check( grid_t *check_1, grid_t *check_2, dimension_t *dim );
+void innit_grid( grid_t * grid, dimension_t * dim, symbols_t * syms );
+void free_grid( grid_t grid, dimension_t dim);
 
 #endif // GAME_H_INCLUDED
