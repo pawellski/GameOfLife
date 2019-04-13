7ce27ed8261a69a9508a36e56d7ce584b9205aab
J-Swider <jakub.swider999@gmail.com>
2019-04-13 13:15:28 +0200

    Poprawki do main.c

--- main.c
+++ main.c
@@ -21,33 +21,30 @@ int main(int argc, char *argv[])
         switch(opt){
         case 'n':
             count_generation = atoi(optarg);
-            printf("count_generation = %d\n", count_generation);
+	    if( count_generation <= 0){
+	    	printf("Ilosc generacji musi byc liczba calkowita wieksza od 0\n");
+		exit(EXIT_FAILURE);
+	    }
             break;
         case 'i':
             fileIn = optarg;
-            printf("fileIn = %s\n", fileIn);
             break;
         case 'o':
             fileOut = optarg;
-            printf("fileOut = %s\n", fileOut);
             break;
         case 'a':
             syms.alive = *optarg;
-            printf("syms.alive = %c\n", syms.alive);
             break;
         case 'd':
             syms.dead = *optarg;
-            printf("syms.dead = %c\n", syms.dead);
             break;
         case 'w':
             writeOpt = atoi(optarg);
             if(writeOpt < 0 || writeOpt > 2){
                 printf("Opcja --w musi byc liczba calkowita z przedzialu <0 ; 2>");
                 exit(EXIT_FAILURE);
-            } else {
-                printf("writeOpt = %d\n", writeOpt);
+            } else 
                 break;
-            }
         case ':':
             printf("Option needs a value");
             break;
@@ -62,9 +59,6 @@ int main(int argc, char *argv[])
     dimension_t static_dimension;
 
     read_dimension( fileIn, &static_dimension, &syms);
-
-    printf("Bez bufora\nWiersze: %d\nKolumny: %d\n", (static_dimension.rows)-2, (static_dimension.columns)-2);
-    printf("Z buforem\nWiersze: %d\nKolumny: %d\n", static_dimension.rows, static_dimension.columns);
 
     innit_grid( &first_grid, &static_dimension, &syms );
     innit_grid( &second_grid, &static_dimension, &syms );
